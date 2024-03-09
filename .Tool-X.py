fed494e9432d597850456b0991cdc57099174e70
Netzeros <162311035+Netzeros@users.noreply.github.com>
2024-03-09 05:48:36 +0100

    Update .Tool-X.py

--- .Tool-X.py
+++ .Tool-X.py
@@ -1,33 +1,34 @@
-# Tool Name :- Tool Kit
-# Author :- Rajkumar Dusad
-# Date :- 28/4/2018
-# Powered By :- Aex Software's
+# Tool Name: Tool Kit
+# Author: Netzeros
+# Date: 28/4/2018
+# Powered By: Aex Software's
 
 import sys
-import os
 from modules.logo import exit
 from modules.menu import ToolX
 
-class chk(object):
-  def chos(self):
-    if "linux" in sys.platform:
-      # Running Tool-X on linux ....
-      pass
-    elif "darwin" in sys.platform:
-      pass
-      # print("Sorry, its not available for mac right now...")
-      ex()
-    elif "win" in sys.platform:
-      print("Sorry, its not available for windows right now...")
-      ex()
-    else:
-      print("If Tool-X not support for \'%s\' right now !!!" %sys.platform)
+class CheckOS(object):
+    def check(self):
+        if "linux" in sys.platform:
+            # Running Tool Kit on Linux ...
+            pass
+        elif "darwin" in sys.platform:
+            # Running Tool Kit on macOS ...
+            pass
+            # print("Sorry, it's not available for macOS right now...")
+            exit()
+        elif "win" in sys.platform:
+            print("Sorry, it's not available for Windows right now...")
+            exit()
+        else:
+            print("Tool Kit does not support '%s' right now!!!" % sys.platform)
 
-def Tool_X():
-  try:
-	chk().chos()
-	ToolX()
+def main():
+    try:
+        CheckOS().check()
+        ToolX()
+    except KeyboardInterrupt:
+        exit()
 
-  except KeyboardInterrupt:
-	exit()
-Tool_X()
+if __name__ == "__main__":
+    main()
